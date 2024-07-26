507b26ca3836045680b4466f018ee11470ea5093
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-07-26 09:47:06 -0700

    changes made

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyDDys15D23w-5E1-pkt9P-TzYiWKYEaPWA",
   authDomain: "sport-injury-bio.firebaseapp.com",
@@ -18,4 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
